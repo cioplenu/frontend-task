5718a393901e09475f88de95d3b7378d9b4720d5
gtoootg0131 <80195266+gtoootg0131@users.noreply.github.com>
2021-09-04 10:31:12 +0200

    create country-details.component

---
+++ src/app/country-details/country-details.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { UnsubscriptionError } from 'rxjs';
+import { CountryListComponent } from '../country-list/country-list.component';
+
+import { Country} from '../country.model';
+import { DataService } from '../data.service';
+import {Observable} from 'rxjs';
+
+
+@Component({
+  selector: 'app-country-details',
+  templateUrl: './country-details.component.html',
+  styleUrls: ['./country-details.component.scss']
+})
+export class CountryDetailsComponent implements OnInit {
+
+  country: Country= {name :"",
+   region :"",
+   capital :"",
+   population :0};
+
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) { }
+
+  ngOnInit(){
+
+    const routeParams = this.route.snapshot.paramMap;
+    const countryNameFromRoute = routeParams.get('countryName');
+
+    return this.dataService.getCountryDetail(countryNameFromRoute)
+    .subscribe(data => {
+      this.country = data[0]
+      console.log(this.country)
+    })
+    
+  }
+
+}

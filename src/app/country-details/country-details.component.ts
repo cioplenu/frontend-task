013bf7b964842308b5743080ee91ff7dc45cc4b5
gtoootg0131 <80195266+gtoootg0131@users.noreply.github.com>
2021-09-05 12:46:06 +0200

    Updated country-detail.component

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -1,33 +1,39 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { ActivatedRoute } from '@angular/router';
-import { UnsubscriptionError } from 'rxjs';
-import { CountryListComponent } from '../country-list/country-list.component';
+import { ActivatedRoute, PreloadingStrategy } from '@angular/router';
+
 
 import { Country} from '../country.model';
 import { DataService } from '../data.service';
-import {Observable} from 'rxjs';
-
+import {Location} from '@angular/common';
+import { CountryListComponent } from '../country-list/country-list.component';
+import { MethodCall } from '@angular/compiler';
 
 @Component({
   selector: 'app-country-details',
   templateUrl: './country-details.component.html',
   styleUrls: ['./country-details.component.scss']
 })
+
+
 export class CountryDetailsComponent implements OnInit {
 
-  country: Country= {name :"",
-   region :"",
-   capital :"",
-   population :0,
-   flag:"",
-   borders:[],
-   alpha3Code:""
-   };
+  country: Country= {
+    name :"",
+    region :"",
+    capital :"",
+    population :0,
+    flag:"",
+    borders:[],
+    alpha3Code:""
+  };
+
+  border: [] =[];
 
   constructor(
     private route: ActivatedRoute,
-    private dataService: DataService
+    private dataService: DataService,
+    private location: Location,
   ) { }
 
   ngOnInit(){
@@ -35,22 +41,30 @@ export class CountryDetailsComponent implements OnInit {
     const routeParams = this.route.snapshot.paramMap;
     const countryNameFromRoute = routeParams.get('countryName');
 
-    return this.dataService.getCountryDetail(countryNameFromRoute)
-    .subscribe(data => {
-      this.country = data[0]
-      console.log(this.country)
-    })
-    
+    return (
+      this.dataService.getCountryDetail(countryNameFromRoute)
+      .subscribe(
+        (data) => {
+            this.country = data[0]
+            console.log(this.country)
+        },
+        (err)=>{
+            alert("Please select Existing Country")
+        }
+    )
+    )
   }
 
-
-  getOfficialCountryName(selectedCountry:string){
-    this.dataService.getCountryList()
-    .subscribe(data=>{
-      const extraction = data.find(country=>country.alpha3Code === selectedCountry)
-      return extraction
-
-    })
+  backBtnClick(){
+    this.location.back()
   }
+  // getOfficialCountryName(selectedCountry:string){
+  //   this.dataService.getCountryList()
+  //   .subscribe(data=>{
+  //     const extraction = data.find(country=>country.alpha3Code === selectedCountry)
+  //     return extraction
+
+  //   })
+  // }
 
 }
